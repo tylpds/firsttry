2e8ebb6d26444762a0ba2393699af02eb4fdb954
admin <admin@admin-PC>
2017-06-09 12:07:37 +0800

    修改main方法

--- jun09_git2/src/test/A.java
+++ jun09_git2/src/test/A.java
@@ -4,6 +4,11 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println(sum(3, 3));
 		print(5);
+		print(5);
+		print(5);
+		print(5);
+		print(5);
+		print(5);
 	}
 	public static int sum(int x,int y) {
 		return x + y;
